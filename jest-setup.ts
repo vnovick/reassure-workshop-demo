bfebc28480e880ee78a10b930667e5d742fd8fdb
Vladimir Novick <vnovick@gmail.com>
2024-01-29 20:21:58 +0200

    no-reassure setup

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,10 +1,8 @@
 import '@testing-library/react-native/extend-expect';
-import {configure} from 'reassure';
+// import {configure} from 'reassure';
 import {jest} from '@jest/globals';
 
 // eslint-disable-next-line no-undef
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 //TODO Configure reassure:
-
-configure({testingLibrary: 'react-native'});
